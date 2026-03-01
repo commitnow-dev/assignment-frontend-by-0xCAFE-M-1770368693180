273fa748f8735a5eadbad0e37e0fdcd375632008
areumH <dkfmal0109@gmail.com>
2026-03-01 15:37:39 +0900

    feat: Button 컴포넌트에 size 값 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,21 @@
+import clsx from 'clsx';
 import type { ComponentPropsWithoutRef } from 'react';
 
 export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   isActive?: boolean;
+  size?: 'md' | 'lg';
 }
 
-const Button = ({ isActive = true, children, ...props }: ButtonProps) => {
+const Button = ({ isActive = true, size = 'md', children, ...props }: ButtonProps) => {
   return (
     <button
       type="button"
       disabled={!isActive}
-      className={`flex justify-center items-center whitespace-nowrap rounded-xl px-4 py-2 text-white  ${isActive ? 'bg-indigo-500 hover:bg-indigo-600 cursor-pointer' : 'bg-gray-400 cursor-not-allowed'}`}
+      className={clsx(
+        'flex items-center justify-center whitespace-nowrap rounded-xl px-4 text-white transition',
+        size === 'lg' ? 'w-full py-3' : 'py-2',
+        isActive ? 'cursor-pointer bg-indigo-500 hover:bg-indigo-600' : 'cursor-not-allowed bg-gray-400',
+      )}
       {...props}
     >
       {children}
--- src/layout/WizardLayout.tsx
+++ src/layout/WizardLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import Button from '@/components/Button';
 
 export default function WizardLayout() {
   return (
     <div className="min-h-screen max-w-lg flex flex-col items-center justify-between px-6 sm:px-16 py-10 mx-auto">
       <p>스텝 인디케이터</p>
       <Outlet />
-      <p>이전/다음 버튼</p>
+      <div className="flex w-full justify-between gap-5">
+        <Button size="lg">이전</Button>
+        <Button size="lg">다음</Button>
+      </div>
     </div>
   );
 }
