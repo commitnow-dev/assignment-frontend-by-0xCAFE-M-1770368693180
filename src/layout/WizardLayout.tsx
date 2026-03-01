8e172de20f39cfcd861f56ba6b26c94e05b84ca6
areumH <dkfmal0109@gmail.com>
2026-03-01 15:54:34 +0900

    style: 위자드 레이아웃 스타일 수정

--- src/layout/WizardLayout.tsx
+++ src/layout/WizardLayout.tsx
@@ -3,12 +3,17 @@ import Button from '@/components/Button';
 
 export default function WizardLayout() {
   return (
-    <div className="min-h-screen max-w-lg flex flex-col items-center justify-between px-6 sm:px-16 py-10 mx-auto">
-      <p>스텝 인디케이터</p>
-      <Outlet />
-      <div className="flex w-full justify-between gap-5">
-        <Button size="lg">이전</Button>
-        <Button size="lg">다음</Button>
+    <div className="min-h-screen max-w-lg flex flex-col items-center mx-auto">
+      <div className="flex w-full justify-center py-10 bg-indigo-100">
+        <p>스텝 인디케이터</p>
+      </div>
+
+      <div className="flex flex-col w-full flex-1 justify-between px-5 sm:px-16 pt-15 pb-10">
+        <Outlet />
+        <div className="flex w-full justify-between gap-5">
+          <Button size="lg">이전</Button>
+          <Button size="lg">다음</Button>
+        </div>
       </div>
     </div>
   );
