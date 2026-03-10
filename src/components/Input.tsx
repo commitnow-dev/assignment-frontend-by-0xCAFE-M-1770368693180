cd70b09f5c509d228c47058784348daf171ebfb9
areumH <dkfmal0109@gmail.com>
2026-03-10 23:53:00 +0900

    feat: 입력 컴포넌트에 id 값 부여

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import type { ComponentPropsWithoutRef } from 'react';
+import { useId } from 'react';
 
 export interface InputProps extends ComponentPropsWithoutRef<'input'> {
   label?: string;
@@ -7,20 +8,27 @@ export interface InputProps extends ComponentPropsWithoutRef<'input'> {
 }
 
 const Input = ({ label, errorMessage, ...props }: InputProps) => {
+  const id = useId();
+
   return (
     <div className="flex flex-col w-full min-w-0 gap-1">
-      {label && <label className="px-1 text-gray-700">{label}</label>}
+      {label && (
+        <label htmlFor={id} className="px-1 text-gray-700">
+          {label}
+        </label>
+      )}
       <div className="relative w-full">
         <input
+          id={id}
           className={clsx(
             'w-full border border-gray-300 bg-white rounded-lg px-3 py-2 outline-none transition',
-            errorMessage ? 'border-red-400 focus:ring-2 focus:ring-red-300' : 'border-gray-300 focus:ring-2 focus:ring-indigo-300',
+            errorMessage
+              ? 'border-red-400 focus:ring-2 focus:ring-red-300'
+              : 'border-gray-300 focus:ring-2 focus:ring-indigo-300',
           )}
           {...props}
         />
-        {errorMessage && (
-          <p className="absolute left-1 top-full pt-1 text-xs text-red-500">{errorMessage}</p>
-        )}
+        {errorMessage && <p className="absolute left-1 top-full pt-1 text-xs text-red-500">{errorMessage}</p>}
       </div>
     </div>
   );
